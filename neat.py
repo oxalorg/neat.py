db50f8c6f91a6b59d5338314e3d6b76d22ef9c67
Mitesh Shah <mitesh@miteshshah.com>
2017-02-05 21:26:15 +0530

    Have to find a bug causing cyclic deps

--- neat.py
+++ neat.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 import random
 import logging
 import pprint
@@ -16,6 +17,7 @@ class Layer(Enum):
 
 def act_fn(z):
     """Sigmoidal activation function"""
+    z = min(40, max(-40, z))
     return (2.0 / (1.0 + math.exp(-4.5 * z))) - 1
 
 def create_gene(ip=None, op=None, wt=0.0, enabled=True, innov_no=0):
@@ -97,8 +99,13 @@ def next_nid(genome):
     return nid
 
 def mutate(genome):
-    mutate_add_node(genome)
-    mutate_add_conn(genome)
+    NODE_MUTATE_PROB = 0.05
+    CONN_MUTATE_PROB = 0.05
+
+    if random.random() < NODE_MUTATE_PROB:
+        mutate_add_node(genome)
+    if random.random() < CONN_MUTATE_PROB:
+        mutate_add_conn(genome)
 
 def mutate_add_conn(g):
     # Select any 2 neurons
@@ -110,7 +117,10 @@ def mutate_add_conn(g):
 
     nid1 = n1['id']
     nid2 = n2['id']
-    # check if a link already exsits
+    # check if a cyclic link exists
+    if detect_cycle(g, nid1, nid2):
+        logging.debug("Found a cycle")
+        return
     if set([(nid1, nid2), (nid2, nid1)]) & set([(x['ip'], x['op']) for x in g['genes'].values()]):
         return
 
@@ -118,8 +128,30 @@ def mutate_add_conn(g):
     gene = create_gene(ip=nid1, op=nid2, wt=1.0, innov_no=innov_no)
     g['genes'][innov_no] = gene
 
+def detect_cycle(g, ip, op):
+    if ip == op:
+        return
+
+    incoming = defaultdict(list)
+    for gene in g['genes'].values():
+        incoming[gene['op']].append(gene['ip'])
+
+    reachable = set([op])
+    unexplored = set([op])
+    explored = set()
+    while unexplored:
+        node = unexplored.pop()
+        explored.add(node)
+        for n in incoming[node]:
+            if n not in explored:
+                reachable.add(n)
+                unexplored.add(n)
+        if ip in reachable:
+            return True
+    return False
+
 def mutate_add_node(g):
-    # Find a random gene
+    # Select any gene
     # Split it into two connections
     gene = random.choice(list(g['genes'].values()))
     if not gene['enabled']:
@@ -140,16 +172,100 @@ def mutate_add_node(g):
     g['genes'][innov_no1] = gene1
     g['genes'][innov_no2] = gene2
 
+def crossover(mom, dad):
+    if mom['fitness'] < dad['fitness']:
+        dad, mom = mom, dad
+
+    child = create_genome()
+    child['neurons'].update(mom['neurons'])
+    child['neurons'].update(dad['neurons'])
+
+    # checks with innovation numbers (which are keys of the gene dict)
+    for gene in mom['genes']:
+        if gene in dad['genes'] and random.random() < 0.5:
+            # matching gene
+            child['genes'][gene] = dad['genes'][gene].copy()
+        else:
+            child['genes'][gene] = mom['genes'][gene].copy()
+
+    for gene in dad['genes']:
+        if gene not in mom['genes']:
+            child['genes'][gene] = dad['genes'][gene].copy()
+
+    last_neuron = max([x['id'] for x in child['neurons'].values()])
+    child['last_neuron'] = last_neuron
+    return child
+
+def create_layers(g):
+    nodep = {x for x in g['ip_neurons']}
+    layers = [nodep.copy()]
+    remaining = {x for x in g['neurons'].keys()} - nodep
+    incoming = defaultdict(list)
+    for gene in g['genes'].values():
+        incoming[gene['op']].append(gene['ip'])
+    while True:
+        L = set()
+        for node in remaining:
+            if set(incoming[node]) <= nodep:
+                nodep.add(node)
+                L.add(node)
+
+        if not L:
+            logging.error("Circular dependency exists")
+            print(remaining)
+            break
+
+        layers.append(L)
+        for node in L:
+            remaining.remove(node)
+
+        if not remaining:
+            break
+
+    return layers
+
+def generate_network(g):
+    layers = create_layers(g)
+    def activate(inputs):
+        # set the values for the inputs
+        values = {x: 0.0 for x in g['neurons'].keys()}
+        for i, ip_n in enumerate(g['ip_neurons']):
+            #g['neurons'][ip_n]['value'] = inputs[i]
+            values[ip_n] = inputs[i]
+
+        incoming = defaultdict(list)
+        wt = {}
+        for gene in g['genes'].values():
+            incoming[gene['op']].append(gene['ip'])
+            wt[(gene['ip'], gene['op'])] = gene['wt']
+
+        for layer in layers[1:]:
+            for node in layer:
+                total = 0
+                for ip in incoming[node]:
+                    total += wt[(ip, node)] * values[ip]
+                total = act_fn(total)
+                values[node] = total
+                print(values)
+
+        for op_n in g['op_neurons']:
+            print(op_n)
+            print(values[op_n])
+
+    return activate
+
 def main():
-    ip = 3
-    op = 2
-    pop_size = 4
+    pop_size = 20
     pop = create_population(pop_size)
+    for _ in range(300):
+        mutate(pop[0])
     print(pop[0])
-    mutate(pop[0])
-    print("~!@~!@~!@~!@~!@~!@~!@~!@~!@~!@~!@~!@")
-    print(pop[0])
+    nw = generate_network(pop[0])
+    nw([-0.55, 0.4, -0.05])
+
+    l = create_layers(pop[0])
+    print(l)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
+    logging.basicConfig(level=logging.DEBUG)
     main()
